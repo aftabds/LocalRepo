1351c4f4fdeea96d825ddb64b78d6d5cd116e609
aftabds <shaikhaftab2401@gmail.com>
2025-03-29 23:13:23 +0530

    Something gone wrong with code

--- first.py
+++ first.py
@@ -1,12 +1,8 @@
 age =  (input("Enter your age: ")) 
 
-print ("You are " + age + " years old")
+print ("You are " + age + "years old")
 
 
 name = input("Enter your name: ")
 
 print ("Hello " + name + "!")
-
-# s ={1, 2, 3, 4, 5}
-# s= {2,3,4,5,6}
-# print (s)
